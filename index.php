cf74e9ced6646eeebfedebdf7d08505a9753118a
Wile TINOCO YURIVILCA <wiletinocoyurivilca@MacBook-Pro-de-Wile.local>
2017-06-19 08:19:29 -0500

    libraries

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Neko</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+<header>
+<nav class="navbar navbar-inverse ">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav pull-right">
+            <li class="active"><a href="#">Cobertura</a></li>
+            <li><a href="#about">Quienes Somos</a></li>
+            <li><a href="#contact">Denunciar Siniestro</a></li>
+          </ul>
+          <ul>
+              <li> <a href="#" class="btn btn-ingresar">Ingresar</a></li>
+              <li>
+              <select>
+                  <option value="#">Es</option>
+                  <option value="#">En</option>
+                 </select>
+              </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+</nav>
+</header>
+<section>
+    <div class="container">
+        <div class="col-md-offset-2 col-md-8">
+            <img src="images/logo2.png" class="img-responsive center-block" alt="Neko">
+        </div>
+         <div class="col-md-offset-2 col-md-8 text-center">
+          <h1>Asegura tu teléfono móvil contra <span>Robo</span>
+de una forma fácil y segura</h1>
+         </div>
+    </div>
+</section>
+<section id="page1" class="fondo1">
+    <div class="container">
+        <div class="col-md-4">
+            <img src="images/iphone.png" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+            <p>Durante todo el proceso nuestro asistente virtual
+te guiará para que obtengas la mejor cobertura
+para tu teléfono</p>
+        </div>
+    </div>
+</section>
+<section id="page2">
+    <div class="container">
+        <div class="col-md-6">
+            <img src="images/cel01.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-6">
+            <h2>Cobertura</h2>
+            <p>Tu teléfono móvil es un artículo preciado para vos,
+nosotros lo aseguramos contra Robo en cualquier parte
+del mundo de forma que puedas reemplazarlo por otro del 
+mismo valor.</p>
+        </div>
+    </div>
+</section>
+
+<section id="page3">
+    <div class="container">
+        <div class="col-md-6">
+           <h2>Como contratar</h2>
+            <p>Para contratar el seguro hay que descargar la app.</p>
+
+<p>Neko te guiará en el proceso, y podrás tener tu celular
+asegurado en menos de 1 minuto con el precio y
+cobertura mas convenientes del mercado.</p>
+        </div>
+        <div class="col-md-6">
+              <img src="images/cel2.jpg" alt="" class="img-responsive">
+           
+        </div>
+    </div>
+</section>
+
+<section id="page4">
+    <div class="container">
+        <div class="col-md-6">
+           <img src="images/siniestro.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-6">
+            <h2>Cómo Denunciar un Siniestro</h2>
+            <p>Una vez asegurado recibirás un e-mail a la casilla de correo 
+                electrónico que indiques. En ese mail tendrás un link personalizado para 
+                ingresar en la página web y realizar la denuncia de siniestro. También podrás 
+                ingresar directamente en la página web "neko.com.ar" e ingresar con tus datos
+                personales</p>
+           
+        </div>
+    </div>
+</section>
+<section id="page5">
+    <div class="container">
+        <div class="col-md-4">
+           <img src="images/dc.png" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-8">
+            <h2>Cómo Denunciar un Siniestro</h2>
+            <p>Una vez asegurado recibirás un e-mail a la casilla de correo 
+                electrónico que indiques. En ese mail tendrás un link personalizado para 
+                ingresar en la página web y realizar la denuncia de siniestro. También podrás 
+                ingresar directamente en la página web "neko.com.ar" e ingresar con tus datos
+                personales</p>
+           
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="container">
+        <div class="col-md-12">
+        <ul>
+            <li><a href="#" class="subnav">Cobertura</a></li>
+            <li><a href="#" class="subnav">Quienes somos</a></li>
+            <li><a href="#" class="subnav">denunciar Siniestro</a></li>
+        </ul>
+        </div>
+         <div class="col-md-12">
+        <ul>
+            <li><a href="#" class="subnav"><i class="fa fa-google" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="subnav"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="subnav"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="subnav"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+        </ul>
+        </div>
+    </div>
+</footer>
+<footer2>
+
+    <div class="container">
+        <div class="col-md-3">
+            <img src="images/seguro.jpg" alt="" class="img-responsive">
+        </div>
+        <div class="col-md-6"></div>
+        <div class="col-md-3">
+            <img src="images/sello.jpg" alt="" class="img-responsive">
+        </div>
+    </div>
+</footer2>
+
+
+        <script scr="node_modules/jquery/dist/jquery.min.js"></script>
+        <script scr="js/vendor/jquery-migrate-1.4.1.min.js"></script>
+        <script scr="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
